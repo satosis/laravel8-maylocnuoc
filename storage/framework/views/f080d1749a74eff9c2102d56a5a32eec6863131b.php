c45bffe1f8b059039a0fc33f54cf8d151de3b2ce
satosis <hung0913003358@gmail.com>
2024-05-15 21:50:43 +0700

    update

--- storage/framework/views/f080d1749a74eff9c2102d56a5a32eec6863131b.php
+++
@@ -1,2 +0,0 @@
-
-<?php echo $__env->make('auth.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\project\tesst\resources\views/auth/register.blade.php ENDPATH**/ ?>
